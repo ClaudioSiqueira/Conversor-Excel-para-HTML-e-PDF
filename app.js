c5dd6bc2e18276304ae075e168e02fae7cc9b2c1
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2020-06-07 18:50:25 -0300

    Criando a classe Processor.
    Essa classe processa os dados, deixando mais faceis de serem manipulados.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let Reader = require('./Reader')
+let Processor = require('./Processor')
 
 let leitor = new Reader()
 
 
 async function main(){
     let dados = await leitor.Read('./dados.csv') // espera a funcao concluir e coloca na variavel
-    console.log(dados)
+    let dadosProcessados = Processor.Process(dados)
 }
+
 
 main()
