ee8580951f6ad36c744ab60e999447c9bd6cfc36
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2020-06-07 19:59:59 -0300

    Classe Table criada

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let Reader = require('./Reader')
 let Processor = require('./Processor')
+let Table = require('./Table')
 
 let leitor = new Reader()
 
@@ -7,6 +8,10 @@ let leitor = new Reader()
 async function main(){
     let dados = await leitor.Read('./dados.csv') // espera a funcao concluir e coloca na variavel
     let dadosProcessados = Processor.Process(dados)
+    
+    var usuarios = new Table(dadosProcessados)
+    console.log(usuarios.rows)
+
 }
 
 
